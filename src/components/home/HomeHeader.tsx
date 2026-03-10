3ef2600e5cf477e5ae9345bc6a2766c6540bd0bd
[dyad] <mktvoeon@gmail.com>
2026-03-10 20:15:14 -0300

    [dyad] Importação do Link corrigida no HomeHeader - wrote 1 file(s)

--- src/components/home/HomeHeader.tsx
+++ src/components/home/HomeHeader.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Menu, X, Search } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';

64fd1ec78a201087769cce32005782be1914b152
[dyad] <mktvoeon@gmail.com>
2026-03-10 20:26:07 -0300

    [dyad] Importação do Link corrigida no PopularProducts - wrote 1 file(s)

--- src/components/home/PopularProducts.tsx
+++ src/components/home/PopularProducts.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { ArrowRight, Filter, Heart, TrendingUp } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import {
